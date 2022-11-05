7f58d067a5545b310958b9ec424adc41cd736d5b
tee52 <onegbebi14@gmail.com>
2022-11-05 19:37:23 -0400

    Part 2

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { render } from 'react-dom';
+import CardEditor from './CardEditor';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            cards: [
+                {front: 'front1', back: 'back1'},
+                {front: 'front2', back: 'back2'},
+            ]
+        }
+    }
+    
+    render() {
+        return <CardEditor cards={this.state.cards} />;
+    }
+    
+}
+
+export default App;
