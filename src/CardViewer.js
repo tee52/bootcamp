b93e14cb58cbc58cddbca6c255690466867f522c
tee52 <onegbebi14@gmail.com>
2022-11-05 21:25:42 -0400

    previous/next buttons on CardViewer

--- src/CardViewer.js
+++ src/CardViewer.js
@@ -5,11 +5,16 @@ class CardViewer extends React.Component {
         super(props);
         this.state = {front: '', back: ''};
     }
-    
+
     render() {
         return (
             <div>
                 <h2>Card Viewer</h2>
+                <div>
+
+                </div>
+                <button>previous card</button>
+                <button>next card</button>
                 <hr/>
                 <button onClick={this.props.switchMode}>go to card editor</button>
             </div>
