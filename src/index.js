a375be2fd735cd682307c9096eaca96584b5a309
tee52 <onegbebi14@gmail.com>
2022-11-26 08:29:52 -0500

    react-router task 3 - attempted to switch versions to react v9 and modify syntax, but not working (check imports)

--- src/index.js
+++ src/index.js
@@ -4,9 +4,55 @@ import App from './App';
 
 import { BrowserRouter } from "react-router-dom";
 
+import { Provider } from 'react-redux'
+import firebase from 'firebase/compat/app'
+import configureStore from '@reduxjs/toolkit'
+import { combineReducers } from 'redux'
+import {
+    ReactReduxFirebaseProvider,
+    firebaseReducer
+} from 'react-redux-firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCr4uFKAOYL2hRcNn2ykAgnLHHgEG4eILY",
+    authDomain: "bootcamp-3062a.firebaseapp.com",
+    databaseURL: "https://bootcamp-3062a-default-rtdb.firebaseio.com",
+    projectId: "bootcamp-3062a",
+    storageBucket: "bootcamp-3062a.appspot.com",
+    messagingSenderId: "562908351044",
+    appId: "1:562908351044:web:68143e84c8b69aaf3c0a11"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+// Add firebase to reducers
+const rootReducer = combineReducers({
+    firebase: firebaseReducer,
+});
+  
+// Create store with reducers and initial state
+const store = configureStore({
+    reducer: rootReducer
+});
+
+// react-redux-firebase config
+const rrfConfig = {
+    userProfile: 'users'
+};  
+
+const rrfProps = {
+    firebase,
+    config: rrfConfig,
+    dispatch: store.dispatch
+};
+
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>, 
+    <Provider store={store}>
+        <ReactReduxFirebaseProvider {...rrfProps}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>, 
+        </ReactReduxFirebaseProvider>
+    </Provider>,
     document.getElementById('root'),
 );
