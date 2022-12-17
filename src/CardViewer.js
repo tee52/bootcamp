aa29fb50a3372ff4e05713261ee5019fe446b8c2
tee52 <onegbebi14@gmail.com>
2022-12-17 12:44:24 -0500

    CardViewer bootcamp pt 4

--- src/CardViewer.js
+++ src/CardViewer.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import './CardViewer.css';
 
-import {Link} from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
+import { firebaseConnect, isLoaded, isEmpty } from 'react-redux-firebase';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
 
 class CardViewer extends React.Component {
     constructor(props) {
@@ -28,9 +31,17 @@ class CardViewer extends React.Component {
     }
     
     render() {
+        if(!isLoaded(this.props.cards)) {
+            return <div>loading...</div>;
+        }
+
+        if(isEmpty(this.props.cards)) {
+            return <div>page not found</div>
+        }
+
         return (
             <div>
-                <h2>Card Viewer</h2>
+                <h2>{this.props.name}</h2>
                 <div className="flashcard" onClick={this.flipCard}>
                     {this.state.front ? this.props.cards[this.state.index].front : this.props.cards[this.state.index].back}
                 </div>
@@ -40,10 +51,26 @@ class CardViewer extends React.Component {
                 </div>
                 <p>Card {this.state.index + 1}/{this.props.cards.length}</p>
                 <hr/>
-                <Link to="/editor">go to card editor</Link>
+                <Link to="/">home</Link>
             </div>
         );
     }
 }
 
-export default CardViewer;
+const mapStateToProps = (state, props) => {
+    console.log(state);
+    const deck = state.firebase.data[props.match.params.deckID];
+    const name = deck && deck.name;
+    const cards = deck && deck.cards;
+    return { cards: cards, name: name };
+}
+
+export default compose(
+    withRouter,
+    firebaseConnect(props => {
+        console.log('props', props);
+        const deckID = props.match.params.deckID;
+        return [{ path: `/flashcards/${deckID}`, storeAs: deckID }]
+    }),
+    connect(mapStateToProps),
+)(CardViewer);
