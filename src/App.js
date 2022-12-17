28823634eda2a0cf274912b0ccbc67b0c2e89d4d
tee52 <onegbebi14@gmail.com>
2022-12-17 13:54:24 -0500

    CardEditor bootcamp pt 4

--- src/App.js
+++ src/App.js
@@ -5,53 +5,23 @@ import Homepage from './Homepage';
 
 import { Switch, Route } from 'react-router-dom';
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            cards: [
-                {front: 'front1', back: 'back1'},
-                {front: 'front2', back: 'back2'},
-            ],
-        };
-    }
-
-    addCard = card => {
-        card.front = card.front.trim();
-        card.back = card.back.trim();
-
-        if (card.front !== '' && card.back !== '')
-        {
-            const cards = this.state.cards.slice().concat(card);
-            this.setState({cards});
-        }
-    };
-
-    deleteCard = index => {
-        const cards = this.state.cards.slice();
-        cards.splice(index, 1);
-        this.setState({cards});
-    }
-    
-    render() {
-        return (
-            <Switch>
-                <Route exact path="/">
-                    <Homepage />
-                </Route>
-                <Route exact path="/editor">
-                    <CardEditor 
-                        cards={this.state.cards} 
-                        addCard={this.addCard} 
-                        deleteCard={this.deleteCard}
-                   />
-                </Route>
-                <Route exact path="/viewer/:deckID">
-                    <CardViewer cards={this.state.cards} />
-                </Route>
-            </Switch>
-        );   
-    }
-}
+const App = () => {
+    return (
+        <Switch>
+            <Route exact path="/">
+                <Homepage />
+            </Route>
+            <Route exact path="/editor">
+                <CardEditor />            
+            </Route>
+            <Route exact path="/viewer/:deckID">
+                <CardViewer />
+            </Route>
+            <Route>
+                <div>page not found</div>
+            </Route>
+        </Switch>
+    );   
+};
 
 export default App;
--- src/CardEditor.js
+++ src/CardEditor.js
@@ -1,26 +1,56 @@
 import React from 'react';
 import './CardEditor.css';
 
-import {Link} from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
+import { firebaseConnect } from 'react-redux-firebase';
+import { compose } from 'redux';
 
 class CardEditor extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {front: '', back: ''};
+        this.state = {
+            cards: [
+                {front: 'front1', back: 'back1'},
+                {front: 'front2', back: 'back2'},
+            ],
+            front: '', 
+            back: '',
+            name: '',
+        };
     }
 
     addCard = () => {
-        this.props.addCard(this.state);
-        this.setState({front: '', back: ''});
-    }
+        if (!this.state.front.trim() || !this.state.back.trim()) {
+            return;
+        }
+        
+        const newCard = { front: this.state.front, back: this.state.back };
+        const cards = this.state.cards.slice().concat(newCard);
+        this.setState({ cards,  front: '', back: '' });
+    };
 
-    deleteCard = index => this.props.deleteCard(index);
+    deleteCard = index => {
+        const cards = this.state.cards.slice();
+        cards.splice(index, 1);
+        this.setState({ cards });
+    };
 
     handleChange = event => 
         this.setState({[event.target.name]: event.target.value});
 
+    createDeck = () => {
+        console.log(this.props.firebase.push('/flashcards').key)
+        const deckID = this.props.firebase.push('/flashcards').key;
+        const newDeck = { cards: this.state.cards, name: this.state.name };
+        const onComplete = () => {
+            console.log('database updated');
+            this.props.history.push(`/viewer/${deckID}`);
+        };
+        this.props.firebase.update(`/flashcards/${deckID}`, newDeck, onComplete);
+    }
+
     render() {
-        const cards = this.props.cards.map((card, index) => {
+        const cards = this.state.cards.map((card, index) => {
             return (
                 <tr key={index}>
                     <td>{card.front}</td>
@@ -35,6 +65,16 @@ class CardEditor extends React.Component {
         return (
             <div>
                 <h2>Card Editor</h2>
+                <div>
+                    Deck name: {'  '}
+                    <input 
+                        name="name"
+                        onChange={this.handleChange} 
+                        placeholder="name of deck" 
+                        value={this.state.name} 
+                    />
+                </div>
+                <br />
                 <table>
                     <thead>
                         <tr>
@@ -50,10 +90,17 @@ class CardEditor extends React.Component {
                 <input name="back" onChange={this.handleChange} placeholder='back of card' value={this.state.back}/>
                 <button onClick={this.addCard}>add card</button>
                 <hr/>
-                <Link to="/viewer">go to card viewer</Link>
+                <div>
+                    <button
+                        disabled={!this.state.name.trim() || this.state.cards.length === 0}
+                        onClick={this.createDeck}
+                    >create deck</button>
+                </div>
+                <br />
+                <Link to="/">home</Link>
             </div>
         );
     }
 }
 
-export default CardEditor;
+export default compose(firebaseConnect([]), withRouter)(CardEditor);
